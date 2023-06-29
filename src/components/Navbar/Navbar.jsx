b1203a5e1689b1cd274b8be8c8a505d03a467a66
Manucher <manucher20056@gmail.com>
2023-06-29 09:14:21 +0500

    .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,24 @@
 //Компоненты
+import { useSwipeRight } from '../../hooks/useSwipeRight'
 import NavigateBottom from './NavigateBottom/NavigateBottom'
 import NavigateCenter from './NavigateCenter/NavigateCenter'
 import NavigateTop from './NavigateTop/NavigateTop'
 
 const Navbar = () => {
+	const touch = useSwipeRight()
+
 	return (
-		<div className="w-20 h-[100vh] flex flex-col justify-between bg-gray-100 dark:bg-dark">
-			<div>
-				<NavigateTop />
-				<NavigateCenter />
-			</div>
-			<NavigateBottom />
-		</div>
+		<>
+			{touch && (
+				<div className="w-20 h-[100vh] flex flex-col justify-between bg-gray-100 dark:bg-dark">
+					<div>
+						<NavigateTop />
+						<NavigateCenter />
+					</div>
+					<NavigateBottom />
+				</div>
+			)}
+		</>
 	)
 }
 
---
+++ src/hooks/useSwipeRight.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+export function useSwipeRight() {
+	const [touchStart, setTouchStart] = useState(0)
+	const [touchEnd, setTouchEnd] = useState(0)
+	const [touch, setTouch] = useState(false)
+
+	const handlerTouchStart = e => {
+		setTouchStart(e.targetTouches[0].clientX)
+	}
+
+	const handlerTouchMove = e => {
+		setTouchEnd(e.targetTouches[0].clientX)
+		checkSwipe()
+	}
+
+	window.addEventListener('touchstart', handlerTouchStart)
+	window.addEventListener('touchmove', handlerTouchMove)
+
+	function checkSwipe() {
+		if (touchEnd - touchStart > 100) {
+			setTouch(true)
+		} else {
+			setTouch(false)
+		}
+	}
+
+	return touch
+}
