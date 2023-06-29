30b97464637690bee52b8c25781a0ea9c4406e58
Manucher <manucher20056@gmail.com>
2023-06-29 09:52:25 +0500

    .

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({ styles = '', type = 'text', onClick, text, disabled = false })
 		<button
 			onClick={btn}
 			type={type}
-			className={`outline-none bg-green rounded-xl py-3 text-white text-base hover:opacity-70 disabled:opacity-60 ${styles}`}
+			className={`outline-none bg-green rounded-xl py-3 text-white text-base hover:opacity-70 disabled:opacity-60 select-none ${styles}`}
 			disabled={disabled}
 		>
 			{text}
