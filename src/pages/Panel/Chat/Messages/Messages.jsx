4dcf36af7f543ec860bfd622973a21db9061a436
Manucher <manucher20056@gmail.com>
2023-06-29 15:46:32 +0500

    .

--- src/pages/Panel/Chat/Messages/Messages.jsx
+++ src/pages/Panel/Chat/Messages/Messages.jsx
@@ -1,12 +1,12 @@
 //Библиотеки
 import { toJS } from 'mobx'
 import { observer } from 'mobx-react-lite'
-import { useEffect, useLayoutEffect, useRef, useState } from 'react'
-//Компоненты
-import TextMessage from './Message/TextMessage'
+import { useEffect,useLayoutEffect,useRef,useState } from 'react'
 //Утилиты
 import ContextMenu from '../../../../components/ContextMenu/ContextMenu'
 import chatController from '../../../../store/chat'
+//Компоненты
+import TextMessage from './Message/TextMessage'
 
 const Messages = observer(() => {
 	const chatMessages = useRef(null)
@@ -40,7 +40,7 @@ const Messages = observer(() => {
 					<span className="text-xl text-green">Похоже что у вас нет ни одного сообщения</span>
 				</div>
 			) : (
-				<div className="w-full h-[calc(100vh-152px)] flex flex-col justify-end overflow-auto bg-gray-100 dark:bg-whiteDark">
+				<div className="w-full h-[calc(100vh-152px)] mint:h-[calc(100vh-192px)] flex flex-col justify-end overflow-auto bg-gray-100 dark:bg-whiteDark">
 					{isContextMenu && <ContextMenu setIsRender={setIsContextMenu} x={x} y={y} messageId={selectMessage} />}
 					<div ref={chatMessages} className="px-5 overflow-y-scroll scroll">
 						{listMessage.map(item => (
